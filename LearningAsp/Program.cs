cdcb00efd2ce806e946621b91e42570bfe1e4c8a
CJO.biz <chrisjo@chrijo.com>
2019-01-06 19:06:52 +0100

    1st update

--- LearningAsp/Program.cs
+++ LearningAsp/Program.cs
@@ -19,6 +19,7 @@ namespace LearningAsp
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+            .UseUrls("http://*:8000")
                 .UseStartup<Startup>();
     }
 }
